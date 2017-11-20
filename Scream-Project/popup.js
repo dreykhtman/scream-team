1fe0031c92a21da6b2149a3408633fac9d5ceca9
confulicious <nicole.a.chu@gmail.com>
2017-11-20 17:13:20 -0500

    data is persisting between refreshes

--- Scream-Project/popup.js
+++ Scream-Project/popup.js
@@ -1,103 +1,4 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
-//  *   is found.
-//  */
-// function getCurrentTabUrl(callback) {
-//   // Query filter to be passed to chrome.tabs.query - see
-//   // https://developer.chrome.com/extensions/tabs#method-query
-//   var queryInfo = {
-//     active: true,
-//     currentWindow: true
-//   };
-
-//   chrome.tabs.query(queryInfo, (tabs) => {
-//     // chrome.tabs.query invokes the callback with a list of tabs that match the
-//     // query. When the popup is opened, there is certainly a window and at least
-//     // one tab, so we can safely assume that |tabs| is a non-empty array.
-//     // A window can only have one active tab at a time, so the array consists of
-//     // exactly one tab.
-//     var tab = tabs[0];
-
-//     // A tab is a plain object that provides information about the tab.
-//     // See https://developer.chrome.com/extensions/tabs#type-Tab
-//     var url = tab.url;
-
-//     // tab.url is only available if the "activeTab" permission is declared.
-//     // If you want to see the URL of other tabs (e.g. after removing active:true
-//     // from |queryInfo|), then the "tabs" permission is required to see their
-//     // "url" properties.
-//     console.assert(typeof url == 'string', 'tab.url should be a string');
-
-//     callback(url);
-//   });
-
-//   // Most methods of the Chrome extension APIs are asynchronous. This means that
-//   // you CANNOT do something like this:
-//   //
-//   // var url;
-//   // chrome.tabs.query(queryInfo, (tabs) => {
-//   //   url = tabs[0].url;
-//   // });
-//   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-// }
-
-// /**
-//  * Change the background color of the current page.
-//  *
-//  * @param {string} color The new background color.
-//  */
-// function changeBackgroundColor(color) {
-//   var script = 'document.body.style.backgroundColor="' + color + '";';
-//   // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-//   // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-//   // into a page. Since we omit the optional first argument "tabId", the script
-//   // is inserted into the active tab of the current window, which serves as the
-//   // default.
-//   chrome.tabs.executeScript({
-//     code: script
-//   });
-// }
-
-// /**
-//  * Gets the saved background color for url.
-//  *
-//  * @param {string} url URL whose background color is to be retrieved.
-//  * @param {function(string)} callback called with the saved background color for
-//  *     the given url on success, or a falsy value if no color is retrieved.
-//  */
-// function getSavedBackgroundColor(url, callback) {
-//   // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-//   // for chrome.runtime.lastError to ensure correctness even when the API call
-//   // fails.
-//   chrome.storage.sync.get(url, (items) => {
-//     callback(chrome.runtime.lastError ? null : items[url]);
-//   });
-// }
-
-// /**
-//  * Sets the given background color for url.
-//  *
-//  * @param {string} url URL for which background color is to be saved.
-//  * @param {string} color The background color to be saved.
-//  */
-// function saveBackgroundColor(url, color) {
-//   var items = {};
-//   items[url] = color;
-//   // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-//   // optional callback since we don't need to perform any action once the
-//   // background color is saved.
-//   chrome.storage.sync.set(items);
-// }
-
-//This function toggles the appearance and disappearance of the settings portion of our extension
-
-
+//expand and shrinking app
 function toggleSettings(){
   let settings = document.getElementById('settings');
   console.log('settings.className= ',settings.className)
@@ -108,46 +9,49 @@ function toggleSettings(){
   }
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
+function getInput() {
+
+  chrome.storage.sync.get(null, function(items) {
+    // var allKeys = Object.keys(items)
+    console.log('this is what comes back', items);
+  })
+}
+
+//wait for DOM to load
 document.addEventListener('DOMContentLoaded', () => {
 
-  let settingsButton = document.getElementById('settings-button');
-  settingsButton.addEventListener('click', () => {
-    console.log('In here?')
-    toggleSettings();
-    getInput()
-  });
+      let settingsButton = document.getElementById('settings-button');
+      settingsButton.addEventListener('click', () => {
+        toggleSettings();
+        getInput()
+      });
 
-});
+    });
 
+//parse url for domain
 function getDomain(url) {
   return url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im)[1];
 }
 
-let blacklist = []
+
+let data = {};
 
 function saveInput(event) {
-  event.preventDefault()
-  let input = document.getElementById('blacklist-url').value;
-  let domain = getDomain(input)
-  blacklist.push(domain)
-chrome.storage.sync.set({"blacklist-url": blacklist}, () => {
-    console.log('this has been saved ', domain)
-  })
-}
 
-function getInput() {
-  // chrome.storage.sync.get("blacklist-url", (domain) => {
-  //   console.log('this is the input',domain)
-  chrome.storage.sync.get('blacklist-url', function(items) {
-    console.log('this is what comes back'   , items);
+  let urlObj = {
+    type: '',
+    goalHrs: 0,
+    goalMins: 0,
+    browsingTime: 0
+  }
+
+  event.preventDefault()
+  let url = getDomain(document.getElementById('blacklist-url').value);
+  urlObj.type = 'redlight'
+  data[url] = urlObj
+  console.log('redlight', data)
+  chrome.storage.sync.set({[url]: data}, () => {
+    console.log('this has been saved ')
   })
 }
 
@@ -155,9 +59,10 @@ function getInput() {
 
 let form = document.getElementById('form');
 
-
 form.addEventListener('submit', (e) => {
+  e.preventDefault()
     saveInput(e)
+    getInput()
 })
 
 

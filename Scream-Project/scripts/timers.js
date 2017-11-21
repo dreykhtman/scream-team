faf87c244dcebbc89dcd00cffbac130cec45cb01
e-r-i-k-a <samuels.erika@yahoo.com>
2017-11-21 16:20:55 -0500

    URL is grabbed and timers strat on DOMContentLoaded

--- Scream-Project/scripts/timers.js
+++ Scream-Project/scripts/timers.js
@@ -1,35 +1,37 @@
 let _startTime;
 let _endTime;
 let browsingTime;
-let _url;
+let _currentUrl;
 
 document.addEventListener('DOMContentLoaded', () => {
-chrome.tabs.onActivated.addListener(() => {
-  _startTime = new Date();
-  getCurrentTabUrl(getDomain);
-});
-
-function getCurrentTabUrl(callback) {
-  let queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-  chrome.tabs.query(queryInfo, (tabs) => {
-    let tab = tabs[0];
-    let url = tab.url;
-    console.assert(typeof url === 'string', 'tab.url should be a string');
-
-    callback(url);
+  chrome.tabs.onActivated.addListener(() => {
+  getCurrentTabUrl(getSiteInfo);
   });
-}
 
-function getDomain(url) {
-  _url = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im)[1];
-}
+  function getCurrentTabUrl(callback) {
+    let queryInfo = {
+      active: true,
+      currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, (tabs) => {
+      let tab = tabs[0];
+      let url = tab.url;
+      console.assert(typeof url === 'string', 'tab.url should be a string');
+      callback(url);
+    });
+  }
 
+  function getSiteInfo(url) {
+    _currentUrl = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im)[1];
+    _startTime = new Date().getTime();
+  }
   // localStorage.setItem('domain', _startTime);
   // firstAlarm();
+
+  console.log(chrome.extension.getURL)
 });
+
+
 
 // function firstAlarm() {
 //   chrome.alarms.create('firstWarning', { delayInMinutes: 0.1 });
@@ -49,7 +51,7 @@ function getDomain(url) {
 //   } else if (Notification.permission === 'granted') {
 //     //notification instance
 //     let notification = new Notification('Hey, you!', {
-//       body: `URL: ${_url}`,
+//       body: `URL: ${_currentUrl}`,
 //       title: 'Hello',
 //       requireInteraction: true
 //     });
@@ -57,7 +59,7 @@ function getDomain(url) {
 //     Notification.requestPermission(permission => {
 //       if (permission === 'granted') {
 //         let notification = new Notification('Hey, you!', {
-//           body: `URL: ${_url}`,
+//           body: `URL: ${_currentUrl}`,
 //           title: 'Hello',
 //           requireInteraction: true
 //         });

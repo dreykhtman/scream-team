4d7308ecab635917840a4679a9c29fcc4b218458
e-r-i-k-a <samuels.erika@yahoo.com>
2017-11-28 18:56:49 -0500

    commit to merge latest master into branch

--- Scream-Project/scripts/bedtime.js
+++ Scream-Project/scripts/bedtime.js
@@ -1,11 +1,15 @@
 function resetBrowsingTime (bedtime, waketime) {
   let currentTime = new Date().toTimeString().split(' ')[0].slice(0,-3);
-  // console.log('currentTime', currentTime)
-  // console.log('bedtime', bedtime);
-  // console.log('waketime', waketime);
+  console.log('currentTime', currentTime)
+  console.log('bedtime', bedtime);
+  console.log('waketime', waketime);
 
   let pastBedtime = currentTime > bedtime
-  let pastWaketime = currentTime > waketime
+  // let pastWaketime = currentTime > waketime
+
+  if (pastBedtime) {
+
+  }
 
   // console.log('pastBedtime', pastBedtime)
   // console.log('pastWaketime', pastWaketime)

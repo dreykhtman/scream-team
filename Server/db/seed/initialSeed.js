1a7d9d03f7af254a9c5f96a7ec0fd7122acb053e
Anna Medyukh <annamedyukh@gmail.com>
2017-11-29 16:17:07 -0500

    wrote seed file for social media package

---
+++ Server/db/seed/initialSeed.js
@@ -0,0 +1,47 @@
+const {db, Package, Site} = require('../');
+
+async function seeder(){
+    await db.sync({force: true});
+    let socialSites = await Site.bulkCreate([
+        {
+            url: 'http://www.instagram.com',
+            goalHrs: 0,
+            goalMins: 15,
+            type: 'RED'
+        },
+        {
+            url: 'http://www.facebook.com',
+            goalHrs: 0,
+            goalMins: 15,
+            type: 'RED'
+        },
+        {
+            url: 'http://www.twitter.com',
+            goalHrs: 0,
+            goalMins: 15,
+            type: 'RED'
+        },
+        {
+            url: 'http://www.reddit.com',
+            goalHrs: 0,
+            goalMins: 15,
+            type: 'RED'
+        }
+    ], {returning: true})
+    let socialPackage = await Package.create({
+        name: 'Social Media Addiction'
+    })
+
+    for(let i = 0; i < socialSites.length; i++){
+        console.log(socialSites[i].id); 
+        await socialPackage.addSite(socialSites[i])
+    }
+
+    await db.close(); 
+}
+
+seeder()
+.then(() => {
+    console.log('seeding complete!');
+})
+

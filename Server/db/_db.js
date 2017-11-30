bde52dd3c7bec4914ae4c19d0322f1025fdfb8b5
Anna Medyukh <annamedyukh@gmail.com>
2017-11-30 15:31:03 -0500

    changed Server/main.js listen

--- Server/db/_db.js
+++ Server/db/_db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize")
 
 //this will be replaced with config information upon deployment.
 const _db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/screamteam", {
-    logging: false
+    logging: true
 })
 
 module.exports = _db;

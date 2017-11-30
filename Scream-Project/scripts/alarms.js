219902d4a09129b7a22964b2fe1fa35b0fe0609e
Dima Reykhtman <neoxdth@gmail.com>
2017-11-30 16:22:34 -0500

    alarms depend on overall browsing time

--- Scream-Project/scripts/alarms.js
+++ Scream-Project/scripts/alarms.js
@@ -1,13 +1,12 @@
 let _blacklistGoals = {};
 let _whiteList = [];
-
+let _totalBrowsingTime = {};
 
 function timeConverter(obj) {
   let hrToSec = obj.goalHrs * 3600;
   let minToSec = obj.goalMins * 60;
   return hrToSec + minToSec;
 }
-
 
 // get goal times from storage
 function goalGetter() {
@@ -23,10 +22,12 @@ function getData() {
     chrome.storage.sync.get(null, (items) => {
       _blacklistGoals = {};
       _whiteList = [];
+      _totalBrowsingTime = {};
       for (let domain in items) {
         if (items.hasOwnProperty(domain)) {
           if (items[domain].type === 'red') {
             _blacklistGoals[domain] = timeConverter(items[domain]);
+            _totalBrowsingTime[domain] = items[domain].browsingTime;
           } else if (items[domain].type === 'green') {
             _whiteList.push(domain);
           }
@@ -44,14 +45,19 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
 // get time in minuts for alarms
 function getBlacklistGoal() {
-  return _blacklistGoals[_currentUrl] / 60;
+  let minutes = (_blacklistGoals[_currentUrl] - _totalBrowsingTime[_currentUrl]) / 60;
+  return minutes;
 }
 
 function firstAlarm() {
-  if (_blacklistGoals.hasOwnProperty(_currentUrl)) {
+  // navigate away if browsing time is greater than goal time
+  if (_totalBrowsingTime[_currentUrl] >= _blacklistGoals[_currentUrl]) {
+    chrome.alarms.clearAll(() => {});
+    let randomUrl = 'http://' + _whiteList[Math.floor(Math.random() * _whiteList.length)];
+    chrome.tabs.update(_currentTabId, { url: randomUrl });
+  } else if (_blacklistGoals.hasOwnProperty(_currentUrl)) {
     chrome.alarms.create('firstWarning', { delayInMinutes: getBlacklistGoal() * 0.5 });
   }
 }
@@ -88,10 +94,16 @@ function notifyMe() {
   }
 }
 
+function uglifier() {
+  document.body.style.backgroundColor = "yellow";
+  document.body.style.fontFamily = "Comic Sans MS, cursive, sans-serif";
+}
+
 // every alarm triggers the next one
 chrome.alarms.onAlarm.addListener(alarm => {
   let randomUrl = 'http://' + _whiteList[Math.floor(Math.random() * _whiteList.length)];
   if (alarm.name === 'firstWarning') {
+    uglifier();
     secondAlarm();
     notifyMe();
   } else if (alarm.name === 'secondWarning') {
--- Scream-Project/scripts/timers.js
+++ Scream-Project/scripts/timers.js
@@ -1,43 +1,51 @@
 let _startTime = {};
 let _currentUrl;
 let _isFocused = {}; // {tabId: boolean}
-let _browsingTime = 0;
+let _browsingTime;
 let _interval;
 let _currentTabId;
 let _stopTime = false;
-let _currentUrlObject = {};
+let _currentUrlObject;
+let _trackUrlTimer = new Set();
 
 // clear storage
 // chrome.storage.sync.clear(() => console.log('all gone!'));
 
 document.addEventListener('DOMContentLoaded', () => {
+
   // starts timer when page loads for the first time
   chrome.tabs.onUpdated.addListener(() => {
+    getBrowsingTime(); // get browsing time for every site from storage
     _stopTime = false;
     getCurrentTabUrl(startTimer);
   });
 
   // stops timer and writes time to storage when page is closed
   chrome.tabs.onRemoved.addListener(() => {
+    chrome.alarms.clearAll(() => {});
     _stopTime = true;
     let newTime = _startTime[_currentUrl] + _browsingTime;
     let addBrowsingTime = {
       browsingTime: newTime,
     };
+    _trackUrlTimer.delete(_currentUrl);
 
     let newObj = Object.assign({}, _currentUrlObject, addBrowsingTime);
     _isFocused[_currentTabId] = false;
+
     chrome.storage.sync.set({ [_currentUrl]: newObj }, () => {
       clearInterval(_interval);
     });
-
-    // _browsingTime = 0;
-    // _currentUrlObject = {};
   });
 
   // checks if tab is active/highlighted
   chrome.tabs.onHighlighted.addListener((highlightInfo) => {
     _isFocused[_currentTabId] = highlightInfo.tabIds[0] === _currentTabId;
+  });
+
+  chrome.tabs.onActivated.addListener(tab => {
+    let { tabId } = tab;
+    _currentTabId = tabId;
   });
 });
 
@@ -50,6 +58,7 @@ function getCurrentTabUrl(callback) {
   chrome.tabs.query(queryInfo, (tabs) => {
     let tab = tabs[0];
     let url = tab.url;
+
     _currentTabId = tab.id;
     console.assert(typeof url === 'string', 'tab.url should be a string');
     callback(url);
@@ -68,13 +77,23 @@ function startTimer(url) {
     return;
   }
 
-  browsingTimeGetter();
-
   _startTime[_currentUrl] = 0;
   _isFocused[_currentTabId] = true;
   _currentUrl = getDomainNoPrefix(url);
+
+  if (_trackUrlTimer.has(_currentUrl)) return;
+  _trackUrlTimer.add(_currentUrl);
+
   firstAlarm(); // initialize alarms from alarms.js
-  _interval = setInterval(countUp, 1000);
+
+  const clearIntervalPromise = new Promise((resolve, reject) => {
+    resolve(clearInterval(_interval));
+  });
+
+  clearIntervalPromise
+    .then(() => {
+      _interval = setInterval(countUp, 1000);
+    });
 }
 
 // increment timer
@@ -83,29 +102,20 @@ function countUp() {
     return;
   }
   if (_isFocused[_currentTabId]) {
-    _startTime[_currentUrl]++;
+    _startTime[_currentUrl] >= 0 ? _startTime[_currentUrl]++ : _startTime[_currentUrl] = 0;
   }
 }
 
 // get url's total time form chrome storage
-function browsingTimeGetter() {
-  getBrowsingTime()
-    .then(time => {
-    });
-}
-
 function getBrowsingTime() {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(null, (items) => {
-      if (items.hasOwnProperty(_currentUrl)) {
-        _currentUrlObject = items[_currentUrl];
-        _browsingTime = _currentUrlObject.browsingTime;
-      } else {
-        chrome.storage.sync.set({ [_currentUrl]: { browsingTime: 0 } }, () => {
-          _browsingTime = 0;
-        });
-      }
-      resolve({ _currentUrlObject, _browsingTime });
-    });
+  chrome.storage.sync.get(null, (items) => {
+    if (items.hasOwnProperty(_currentUrl)) {
+      _currentUrlObject = items[_currentUrl];
+      _browsingTime = items[_currentUrl].browsingTime;
+    } else {
+      chrome.storage.sync.set({ [_currentUrl]: { browsingTime: 0 } }, () => {
+        _browsingTime = 0;
+      });
+    }
   });
 }
